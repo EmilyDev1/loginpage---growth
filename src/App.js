dcd1f0ff574842e44720bf3af2ce129a1c629fce
EmilyDev1 <sheismunashe@gmail.com>
2024-11-12 14:27:13 +0200

    first commit1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className= 'text-blue-700 text-3xl ml-5 mt-10 flex justify-center bg-red-300'>
+     
+    </div>
+  );
+}
+
+export default App;
